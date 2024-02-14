6f09871ad9637931bad98c5c4f71449d42c17aaa
aiturann <aiturgannnnnn@gmail.com>
2024-02-14 07:09:27 +0600

    Initial Commit

---
+++ 4lesson2LayOut/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  4lesson2LayOut
+//
+//  Created by Aiturgan Kurmanbekova on 14/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
